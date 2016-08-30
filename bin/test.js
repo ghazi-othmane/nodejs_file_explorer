f18f1bfb189dff492ec7d05f4e8ecf51d877809f
Your Name <ghazi.othmane@gmail.com>
2016-08-30 17:02:45 +0200

    update server config and add new modules

--- bin/test.js
+++ bin/test.js
@@ -1,3 +1,23 @@
-/**
- * Created by V on 30/08/2016.
- */
+var fs = require('fs');
+var url = require('url');
+
+var absPath, reqPath, path, vData;
+
+// Building read path
+absPath = '/tmp/';
+reqPath = process.argv[2];
+path = absPath + reqPath
+
+// Listings files & dirs in path
+fs.readdir(path, function(err, items){
+
+    if(err) {
+        console.log(err);
+        return;
+    }
+    // Rendering files & dirs list
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+
+});
